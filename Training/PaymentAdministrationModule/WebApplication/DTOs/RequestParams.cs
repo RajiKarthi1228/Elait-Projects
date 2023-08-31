fd441699b06c983a21ac4108f1d634a3d9a8a455
RajiKarthi1228 <141731630+RajiKarthi1228@users.noreply.github.com>
2023-08-31 12:06:16 +0530

    Delete Training directory

--- Training/PaymentAdministrationModule/WebApplication/DTOs/RequestParams.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication.DTOs
-{
-    public class RequestParams
-    {
-        public int StartLimit { get; set; }
-        public int EndLimit { get; set; }
-        public int ItemsPerPage { get; set; }
-    }
-}
